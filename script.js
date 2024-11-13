013bb56c7759669c52d1af8900b579c014e021d6
Mohammad Nadhim Azeez <65634550+MohammadNadhimAzeez@users.noreply.github.com>
2024-11-13 23:17:15 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+// Example: Smooth Scroll for navigation links
+document.querySelectorAll('.nav-links a').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+      e.preventDefault();
+      document.querySelector(this.getAttribute('href')).scrollIntoView({
+        behavior: 'smooth'
+      });
+    });
+  });  
